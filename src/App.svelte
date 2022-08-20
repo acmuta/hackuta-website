acdfea8a74f6823fdaf4a4f574d9439128d66a4f
Ryan Le <31494954+lryanle@users.noreply.github.com>
2022-08-20 17:30:32 -0500

    Revert "Splash"

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,40 @@
 <script>
-  import hackLogo from "./assets/logo.png";
-  import backgroundVid from "./assets/background.mp4";
-  import "./lib/Countdown.svelte";
-  // import ArrowDown from 'svelte-material-icons/ChevronDown.svelte';
-  // import Navbar from './lib/Navbar.svelte'
-  // import Band from './lib/Band.svelte'
-  import LinkBtn from "./lib/LinkBtn.svelte";
-  import Countdown from "./lib/Countdown.svelte";
-  import acmLogo from "./assets/acm_logo.png";
+	import hackLogo from './assets/logo.png'
+	import backgroundVid from './assets/background.mp4'
+	import './lib/Countdown.svelte'
+	import ArrowDown from 'svelte-material-icons/ChevronDown.svelte';
+	import Navbar from './lib/Navbar.svelte'
+	import Band from './lib/Band.svelte'
+	import LinkBtn from './lib/LinkBtn.svelte'
+	import Countdown from './lib/Countdown.svelte'
+	import acmLogo from './assets/acm_logo.png'
 
-  let navLinks = [
-    { name: "Home", url: "#" },
-    { name: "About", url: "#about" },
-    { name: "Schedule", url: "#schedule" },
-    { name: "FAQs", url: "#faq" },
-    { name: "Sponsors", url: "#sponsors" },
-  ];
+	let navLinks = [
+		{name: "Home", url:"#"},
+		{name: "About", url:"#about"},
+		{name: "Schedule", url:"#schedule"},
+		{name: "FAQs", url: "#faq"},
+		{name: "Sponsors", url: "#sponsors"}
+	]
 </script>
 
 <main>
-  <!-- <Navbar links={navLinks}></Navbar> -->
-  <div class="no-overflow">
-    <video class="topBg hero" src={backgroundVid} autoplay muted loop />
-  </div>
-  <div class="hero-content">
-    <img class="logo" src={hackLogo} alt="HackUTA Logo" />
-    <h1>📅 October 1-2, 2022</h1>
-    <Countdown
-      from="October 1 2022 00:00:00 UTC-0600"
-      to="October 2 2021 23:59:59 UTC-0600"
-    />
-    <br />
-    <div>
-      <LinkBtn href="https://discord.gg/zND7JCAN8y">HackUTA Discord</LinkBtn>
-      <!-- <LinkBtn href="/">Visit us on DevPost</LinkBtn> -->
-    </div>
-    <!-- <p>Registration opens soon!</p> -->
-    <!-- <a href="#downbtn" id="downbtn"><ArrowDown color="white" size="4rem"></ArrowDown></a> -->
-  </div>
-  <!-- <Band id="about">
+	<Navbar links={navLinks}></Navbar>
+	<video class="topBg hero" src="{backgroundVid}" autoplay muted loop></video>
+		<div class="hero-content">
+			<img src="{hackLogo}" alt="HackUTA Logo">
+			<h1>📅 October 1-2, 2022</h1>
+			<Countdown from="October 1 2022 00:00:00 UTC-0600" to="October 2 2021 23:59:59 UTC-0600"></Countdown>
+			<br>
+			<div>
+				<LinkBtn href="https://discord.gg/zND7JCAN8y">Join the HackUTA Discord</LinkBtn>
+				<LinkBtn href="/">Visit us on DevPost</LinkBtn>
+			</div>
+			<!-- <p>Registration opens soon!</p> -->
+			<a href="#downbtn" id="downbtn"><ArrowDown color="white" size="4rem"></ArrowDown></a>
+			
+		</div>
+	<Band id="about">
 		<h1 slot="heading">About HackUTA</h1>
 		<div slot="content">
 			<p>
@@ -67,70 +63,48 @@
 		<div slot="content">
 			<p>Looking to sponsor us? Reach out to us at .</p>
 		</div>
-	</Band> -->
+	</Band>
 </main>
 
 <style lang="scss">
-  // .hero {
-  // 	height: 100vh!important; // Trust me, the !important is necessary here
-  // 	width: auto!important; // my bad, it wasn't - media queries >>>
-  // }
-  @media only screen and (max-width: 390px) {
-    .topBg {
-      filter: blur(2px);
-    }
+	// .hero {
+	// 	height: 100vh!important; // Trust me, the !important is necessary here
+	// 	width: auto!important; // my bad, it wasn't - media queries >>>
+	// }
+	@media(min-aspect-ratio: 16/9) {
+		.hero {
+			width: 100%;
+			height: auto;
+		}
+	}
+	@media(max-aspect-ratio: 16/9) {
+		.hero {
+			height: 100%;
+			width: auto;
+		}
+	}
+	#downbtn {
+		position: relative;
+		display: inline-block;
+		top: 2rem;
+	}
+	.hero-content {
+		h1 {
+			font-weight: 300;
+		}
+		color: white;
+		position: absolute;
+		margin: auto;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		text-align: center;
 
-    //smaller date text
-    h1 {
-      font-size: 1.5rem;
-    }
-
-    //bigger logo text
-    .logo {
-      width: 80vw !important;
-    }
-  }
-
-  @media (min-aspect-ratio: 16/9) {
-    .hero {
-      width: 100vh;
-      height: auto;
-    }
-  }
-  @media (max-aspect-ratio: 16/9) {
-    .hero {
-      height: 100vh;
-      width: auto;
-    }
-  }
-
-  .no-overflow {
-    overflow: hidden;
-  }
-
-  #downbtn {
-    position: relative;
-    display: inline-block;
-    top: 2rem;
-  }
-  .hero-content {
-    h1 {
-      font-weight: 300;
-    }
-    color: white;
-    position: absolute;
-    margin: auto;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    width: 80%;
-
-    img {
-      width: 40vw;
-      height: auto;
-      display: block;
-      margin: auto;
-    }
-  }
+		img {
+			width: 60%;
+			height: auto;
+			display: block;
+			margin: auto;
+		}
+	}
 </style>
--- src/lib/Countdown.svelte
+++ src/lib/Countdown.svelte
@@ -1,106 +1,90 @@
 <script>
-  export let from;
-  export let to;
-  let total = 0,
-    days = 0,
-    hours = 0,
-    minutes = 0,
-    seconds = 0;
-  let finished = false;
+    export let from;
+    export let to;
+    let total = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
+    let finished = false;
 
-  function getTimeRemaining(endtime) {
-    total = Date.parse(from) - Date.parse(new Date().toString());
-  }
+    function getTimeRemaining(endtime){
+        total = Date.parse(from) - Date.parse(new Date().toString());
+    }
 
-  const now = Date.parse(new Date().toString());
-  if (Date.parse(to) - now > 0) {
-    finished = true;
-  }
+    const now = Date.parse(new Date().toString())
+    if(Date.parse(to) - now > 0)
+    {
+        finished = true;
+    }
 
-  $: seconds = Math.floor((total / 1000) % 60);
-  $: minutes = Math.floor((total / 1000 / 60) % 60);
-  $: hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  $: days = Math.floor(total / (1000 * 60 * 60 * 24));
+    $: seconds = Math.floor( (total/1000) % 60 );
+    $: minutes = Math.floor( (total/1000/60) % 60 );
+    $: hours = Math.floor( (total/(1000*60*60)) % 24 );
+    $: days = Math.floor( total/(1000*60*60*24) );
 
-  const UpdateTimer = setInterval(() => {
-    getTimeRemaining(from);
-  }, 1000);
+    const UpdateTimer = setInterval(() => {getTimeRemaining(from)}, 1000);
+
 </script>
-
 <div id="countdown">
-  {#if total >= 0}
+    {#if total >= 0}
     <div>
-      <span class="big days"
-        >{days.toLocaleString("en-US", { minimumIntegerDigits: 2 })}</span
-      >
-      <div class="small">Days</div>
+        <span class="big days">{days.toLocaleString('en-US', {minimumIntegerDigits: 2})}</span>
+        <div class="small">Days</div>
     </div>
     <span class="colon">:</span>
     <div>
-      <span class="big hours"
-        >{hours.toLocaleString("en-US", { minimumIntegerDigits: 2 })}</span
-      >
-      <div class="small">Hours</div>
+        <span class="big hours">{hours.toLocaleString('en-US', {minimumIntegerDigits: 2})}</span>
+        <div class="small">Hours</div>
     </div>
     <span class="colon">:</span>
     <div>
-      <span class="big minutes"
-        >{minutes.toLocaleString("en-US", { minimumIntegerDigits: 2 })}</span
-      >
-      <div class="small">Minutes</div>
+        <span class="big minutes">{minutes.toLocaleString('en-US', {minimumIntegerDigits: 2})}</span>
+        <div class="small">Minutes</div>
     </div>
     <span class="colon">:</span>
     <div>
-      <span class="big seconds"
-        >{seconds.toLocaleString("en-US", { minimumIntegerDigits: 2 })}</span
-      >
-      <div class="small">Seconds</div>
+        <span class="big seconds">{seconds.toLocaleString('en-US', {minimumIntegerDigits: 2})}</span>
+        <div class="small">Seconds</div>
     </div>
-  {:else if !finished}
-    <div class="finished">Ended</div>
-  {:else}
-    <div class="happening">Now active</div>
-  {/if}
+    {:else if !finished}
+        <div class="finished">Ended</div>
+    {:else}
+        <div class="happening">Now active</div>
+    {/if}
 </div>
 
 <style lang="scss">
-  #countdown {
-    text-align: center;
-    padding: 2vh 0 2vh;
-    @media only screen and (max-width: 390px) {
-		.big {
-			display: block;
-            font-size: 1rem;
+#countdown
+{
+	text-align: center;
+	> div
+	{
+		display: inline-block;
+		
+		.big
+		{
+		    font-size: 2rem;
 		}
-        .colon {
-            padding: 0 .2rem !important;
-        }
+		.small
+		{
+			font-size: 1rem;
+			opacity: 0.65;
+		}
 	}
-    > div {
-      display: inline-block;
-
-      .big {
-        font-size: 2rem;
-      }
-      .small {
-        font-size: 1rem;
-        opacity: 0.65;
-      }
-    }
-    .colon {
-      font-size: 4rem;
-      padding: 0 1rem;
-      position: relative;
-      top: -0.25rem;
-      opacity: 0.85;
-    }
-    .happening {
-      color: lime;
-      font-size: 2.5rem;
-    }
-    .finished {
-      color: white;
-      font-size: 2.5rem;
-    }
-  }
+	.colon
+	{
+		font-size: 4rem;
+		padding: 0 1rem;
+		position: relative;
+		top: -0.25rem;
+		opacity: 0.85;
+	}
+	.happening
+	{
+		color: lime;
+        font-size: 2.5rem;
+	}
+	.finished
+	{
+		color: white;
+        font-size: 2.5rem;
+	}
+}
 </style>
