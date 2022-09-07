a8c5e39915e76179117e873be089c0b56263195a
Aryan Mediratta <aryan_02@outlook.com>
2022-09-07 05:18:57 -0500

    Added event schedule

---
+++ src/lib/events_list.js
@@ -0,0 +1,205 @@
+let workshop = "#FE6B64"
+let sideEvent = "#B29DD9"
+let other = "#779ECB"
+export let events = [
+    {
+        title: "Hacking Period",
+        start: '2022-10-01 12:00:00',
+        end: '2022-10-02 12:00:00',
+        color: other,
+        allDay: true
+    },
+    {
+        title: "Hacking Starts",
+        start: '2022-10-01 12:00:00',
+        end: '2022-10-01 12:00:00',
+        color: other,
+        //allDay: true
+    },
+    {
+        title: "Python Workshop with Prof. Donna French",
+        start: '2022-09-10 13:00:00',
+        end: '2022-09-10 16:00:00',
+        color: workshop
+    },
+    {
+        title: "Intro to Web Development with Mobi",
+        start: '2022-09-23 17:00:00',
+        end: '2022-09-23 19:00:00',
+        color: workshop
+    },
+    {
+        title: "C# Workshop with Paycom",
+        start: '2022-09-26 12:00:00',
+        end: '2022-09-26 13:00:00',
+        color: workshop
+    },
+    {
+        title: "Check-in",
+        start: '2022-10-1 10:00:00',
+        end: '2022-10-1 11:30:00',
+        color: other
+    },
+    {
+        title: "Typing Contest",
+        start: '2022-10-1 10:30:00',
+        end: '2022-10-1 11:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Opening Ceremony",
+        start: '2022-10-1 11:30:00',
+        end: '2022-10-1 12:00:00',
+        color: other
+    },
+    {
+        title: "Lunch",
+        start: '2022-10-1 12:00:00',
+        end: '2022-10-1 13:00:00',
+        color: other
+    },
+    {
+        title: "Machine Learning Workshop",
+        start: '2022-10-1 13:00:00',
+        end: '2022-10-1 14:00:00',
+        color: workshop
+    },
+    {
+        title: "Bob Ross MS Paint",
+        start: '2022-10-1 13:30:00',
+        end: '2022-10-1 14:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Computer Vision Workshop",
+        start: '2022-10-1 14:00:00',
+        end: '2022-10-1 15:00:00',
+        color: workshop
+    },
+    {
+        title: "Skribbl.io",
+        start: '2022-10-1 14:30:00',
+        end: '2022-10-1 15:30:00',
+        color: sideEvent
+    },
+    {
+        title: "APIs Workshop",
+        start: '2022-10-1 15:00:00',
+        end: '2022-10-1 16:00:00',
+        color: workshop
+    },
+    {
+        title: "Hungry Hungry Hippos",
+        start: '2022-10-1 15:30:00',
+        end: '2022-10-1 16:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Databases Workshop",
+        start: '2022-10-01 16:00:00',
+        end: '2022-10-01 17:00:00',
+        color: workshop
+    },
+    {
+        title: "BinarySearch",
+        start: '2022-10-1 16:30:00',
+        end: '2022-10-1 17:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Portfolio Workshop",
+        start: '2022-10-1 17:00:00',
+        end: '2022-10-1 18:00:00',
+        color: workshop
+    },
+    {
+        title: "Cup Stacking",
+        start: '2022-10-1 17:30:00',
+        end: '2022-10-1 18:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Dinner",
+        start: '2022-10-1 19:00:00',
+        end: '2022-10-1 20:00:00',
+        color: other
+    },
+    {
+        title: "Slideshow Karaoke",
+        start: '2022-10-1 19:30:00',
+        end: '2022-10-1 20:30:00',
+        color: sideEvent
+    },
+    {
+        title: "CTF",
+        start: '2022-10-1 20:00:00',
+        end: '2022-10-1 21:00:00',
+        color: sideEvent
+    },
+    {
+        title: "!Light",
+        start: '2022-10-1 20:30:00',
+        end: '2022-10-1 21:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Drone Building",
+        start: '2022-10-1 21:00:00',
+        end: '2022-10-1 22:00:00',
+        color: sideEvent
+    },
+    {
+        title: "Multiplayer Games",
+        start: '2022-10-1 22:30:00',
+        end: '2022-10-1 23:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Minecraft Server Challenge",
+        start: '2022-10-1 22:00:00',
+        end: '2022-10-1 22:50:00',
+        color: sideEvent
+    },
+    {
+        title: "Super Smash Bros Ultimate",
+        start: '2022-10-1 23:00:00',
+        end: '2022-10-2 01:00:00',
+        color: sideEvent
+    },
+    {
+        title: "Talent Show, Karaoke",
+        start: '2022-10-2 00:00:00',
+        end: '2022-10-2 01:30:00',
+        color: sideEvent
+    },
+    {
+        title: "Breakfast",
+        start: '2022-10-2 09:00:00',
+        end: '2022-10-2 09:30:00',
+        color: other
+    },
+    {
+        title: "Yoga",
+        start: '2022-10-2 10:00:00',
+        end: '2022-10-2 11:00:00',
+        color: sideEvent
+    },
+    {
+        title: "Lunch",
+        start: '2022-10-2 12:00:00',
+        end: '2022-10-2 13:00:00',
+        color: other
+    },
+    {
+        title: "Judging",
+        start: '2022-10-2 12:30:00',
+        end: '2022-10-2 15:30:00',
+        color: other
+    },
+    {
+        title: "Closing Ceremony",
+        start: '2022-10-2 15:30:00',
+        end: '2022-10-2 16:30:00',
+        color: other
+    },
+];
